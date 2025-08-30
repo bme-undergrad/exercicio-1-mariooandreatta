ac03958c5f7641ae76c90a97b3501d34bb337e5e
mariooandreatta <mariooandreatta@gmail.com>
2025-08-30 16:03:33 -0300

    exercicio1.m V4

--- exercicio1.m
+++ exercicio1.m
@@ -1,56 +1,42 @@
-function t = exercicio1(func, x0)
+function t = exercicio1(func,x0)
+
 % nao alterar: inicio
 es = 1;
 imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-% digite seu codigo aqui
 
-% Método da Secante
 iter = 0;
-x_prev = x0;
-x_curr = x0 * 1.01; % Segundo ponto inicial
+xr = x0;
 ea = 100;
+delta = 1e-6;
 
 while (ea > es && iter < imax)
+    
+    xr_old = xr;
+    
+    fx_perturbed = func(xr + delta * xr);
+    fx = func(xr);
+    
+    derivada_aprox = (fx_perturbed - fx) / (delta * xr);
+    
+    if derivada_aprox ~= 0
+        xr = xr - fx / derivada_aprox;
+    else
+        break;
+    end
+    
     iter = iter + 1;
     
-    % Avaliar a função nos pontos atuais
-    f_prev = func(x_prev);
-    f_curr = func(x_curr);
-    
-    % Evitar divisão por zero
-    if abs(f_curr - f_prev) < 1e-12
-        break;
+    if xr ~= 0
+        ea = abs((xr - xr_old) / xr) * 100;
     end
     
-    % Calcular nova estimativa
-    x_new = x_curr - f_curr * (x_curr - x_prev) / (f_curr - f_prev);
-    
-    % Não permitir valores negativos
-    if x_new < 0
-        x_new = 1e-10;
-    end
-    
-    % Calcular erro relativo percentual
-    if abs(x_new) > 1e-12
-        ea = abs((x_new - x_curr) / x_new) * 100;
-    else
-        ea = 0;
-    end
-    
-    % Atualizar pontos para próxima iteração
-    x_prev = x_curr;
-    x_curr = x_new;
-    
-    % Critério de parada adicional
-    if abs(func(x_curr)) < 1e-10
-        break;
-    end
 end
 
-t = x_curr;
+t = xr;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
+
 endfunction
