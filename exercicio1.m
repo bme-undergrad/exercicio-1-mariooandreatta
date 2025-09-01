2dcbee2b6a9e72756b3ca4468f81f3fe3bc71c55
mariooandreatta <mariooandreatta@gmail.com>
2025-09-01 19:13:48 -0300

    exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,4 +1,4 @@
-function t = exercicio1(func,d_func,x0)
+function t = exercicio1(func,func_d,x0)
 
 % nao alterar: inicio
 es = 0.01;
@@ -8,18 +8,19 @@ imax = 20;
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 t_roots = zeros(imax,1);
-t_roots(1) = x0; 
+t_roots(1) = x0;
 
 for ii = 1:length(t_roots)-1
-if ii ~= 1
-erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii))
-if erro(ii) < es
-break
-endif
-endif
-t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii));
+  if ii ~= 1
+    erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii))
+    if erro(ii) < es
+      break
+    endif
+  endif
+    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii));
 endfor
-t = t_roots(ii);
+
+t = t_roots(ii); 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
