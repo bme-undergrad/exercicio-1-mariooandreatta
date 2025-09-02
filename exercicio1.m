68e117b6b73b38d5d3169763db6efed29d0ce21e
mariooandreatta <mariooandreatta@gmail.com>
2025-09-02 11:21:19 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -17,9 +17,8 @@ for ii = 1:length(t_roots)-1
       break
     endif
   endif
-    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii));
+    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii))
 endfor
-
 t = t_roots(ii); 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
